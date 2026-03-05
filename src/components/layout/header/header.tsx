76ae51255967dd43a8af4313fbb5873d0389304a
pietr <pietrokucharski@hotmail.com>
2026-03-05 15:00:55 -0300

    refactor: refactor pages and components

--- src/components/layout/header/header.tsx
+++ src/components/layout/header/header.tsx
@@ -1,7 +1,7 @@
+import ActiveLink from "@/components/active-link/active-link";
+import { Logo } from "@/components/logo/logo";
+import { Button } from "@/components/ui/button";
 import Link from "next/link";
-import { Button } from "../ui/button";
-import ActiveLink from "../active-link/active-link";
-import { Logo } from "../logo/logo";
 
 export const Header = () => {
   return (
@@ -9,7 +9,7 @@ export const Header = () => {
       className="fixed top-0 z-50 w-full border-b border-white/10 bg-background/95 backdrop-blur 
     supports-backdrop-filters:bg-background/60"
     >
-      <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-80">
+      <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
         <div className="flex h-16 items-center justify-between">
           <Logo />
 
--- src/templates/landing-page/sections/hero-section/hero-section.tsx
+++ src/templates/landing-page/sections/hero-section/hero-section.tsx
@@ -1,7 +1,7 @@
-import { ArrowRight, Clock, Store } from 'lucide-react';
-import Image from 'next/image';
-import Link from 'next/link';
-import { Button } from '../ui/button';
+import { ArrowRight, Clock, Store } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+import { Button } from "../../../../components/ui/button";
 
 export const HeroSection = () => {
   return (
@@ -16,14 +16,14 @@ export const HeroSection = () => {
             <div>
               <div className="flex items-center gap-2">
                 <Clock className="text-cyan-100 h-4 w-4" />
-                <span className="text-gray-200">
+                <span className="text-body-md text-gray-200">
                   Crie o seu site em menos de 5 minutos
                 </span>
               </div>
 
               <div className="flex items-center gap-2">
                 <Store className="text-cyan-100 h-4 w-4" />
-                <span className="text-gray-200">
+                <span className="text-body-md text-gray-200">
                   Acompanhe e otimize seu negócio online
                 </span>
               </div>
@@ -44,7 +44,7 @@ export const HeroSection = () => {
           </div>
         </div>
 
-        <div className="relative h-80 hidden md:h-full order-first md:order-last items-center justify-center md:flex lg:flex">
+        <div className="relative h-[20rem] hidden md:h-full order-first md:order-last items-center justify-center md:flex lg:flex">
           <Image
             src="/hero-section.png"
             alt="Ilustração com ícones de store, tag e sacola"
